fa74ad858fb0252e5456d3d635e4f96c0583ffa6
Cesar Andia <zarek2807@gmail.com>
2023-12-31 19:11:52 -0500

    add conection to database in mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true
+    }),MongooseModule.forRoot(process.env.MONGODB_URI, {
+  
+    }), UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
